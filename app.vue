1b90cda5ed620e1b89c516908831968f367b9d53
Peter Kubasov <kubasov@gmail.com>
2024-04-01 18:45:34 +0300

    add registration in Firebase

--- app.vue
+++ app.vue
@@ -1,5 +1,45 @@
+<script setup lang="ts">
+const { registerUser } = useFirebaseAuth();
+
+const user = reactive({
+	email: "",
+	password: ""
+})
+
+async function handleRegistration() {
+	console.log(user.email, user.password)
+	await registerUser(user.email, user.password);
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+	  <div class="login_form">
+		  <form>
+			  <fieldset>
+				  <legend>Войти</legend>
+			  </fieldset>
+
+			  <input v-model="user.email" type="email" name="email" id="">
+			  <input v-model="user.password" type="password" name="password" id="">
+
+			  <button @click.prevent="handleRegistration">Войти</button>
+		  </form>
+	  </div>
+
+	  <div class="authorization_form">
+		  <fieldset>
+			  <legend>Зарегистрироваться</legend>
+		  </fieldset>
+		  <input type="email" name="" id="">
+		  <input type="password" name="" id="">
+	  </div>
+
+	  <NuxtWelcome />
   </div>
 </template>
+
+<style lang="stylus">
+input
+	border 1px solid coral
+</style>
---
+++ plugins/firebase.client.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from "firebase/auth"
+import { getFirestore } from 'firebase/firestore'
+import { getAnalytics } from "firebase/analytics"
+
+export default defineNuxtPlugin(nuxtApp => {
+	const config = useRuntimeConfig()
+
+	const firebaseConfig = {
+		// measurementId: config.FB_MEASUREMENT_ID
+		apiKey: "AIzaSyAylZ5F-8x8ohF7Sx22H3_pKsaMESrxEXY",
+		authDomain: "xtray-nuxt3.firebaseapp.com",
+		projectId: "xtray-nuxt3",
+		storageBucket: "xtray-nuxt3.appspot.com",
+		messagingSenderId: "879117160994",
+		appId: "1:879117160994:web:dd41e9c564a8232305307b"
+	};
+
+	const app = initializeApp(firebaseConfig)
+
+	const analytics = getAnalytics(app)
+	const auth = getAuth(app)
+	const firestore = getFirestore(app)
+
+	nuxtApp.vueApp.provide('auth', auth)
+	nuxtApp.provide('auth', auth)
+
+	nuxtApp.vueApp.provide('firestore', firestore)
+	nuxtApp.provide('firestore', firestore)
+})
+
+// proof https://medium.com/codex/using-firebase-in-nuxt3-the-right-way-bebbb6d8c4dd
