5f8d894ef9d3cf44d1f5fba1e2d8ed2c63cfa4bf
Peter Kubasov <kubasov@gmail.com>
2024-04-10 17:09:08 +0300

    add Task list

--- app.vue
+++ app.vue
@@ -29,7 +29,8 @@ async function exit() {
 </script>
 
 <template>
-  <div>
+  <main class="grid">
+	  <section class="content">
 	  <div class="login_form">
 		  <form>
 			  <fieldset>
@@ -52,15 +53,60 @@ async function exit() {
 		  </fieldset>
 	  </div>
 
-	  <button @click.prevent="who">Who</button>
+	  <div class="authorization_form">
+		  <fieldset>
+			  <legend>Получить данные пользователя</legend>
+			  <button @click.prevent="who">Who</button>
+		  </fieldset>
+	  </div>
+
 	  <button @click.prevent="exit">Выйти</button>
 
 	  <h2 v-if="isLogin">Вы вошли как {{ userAcc.user.email }}</h2>
 	  <h2 v-else>Вы не вошли</h2>
-  </div>
+	  </section>
+
+	  <aside>
+		  <h3>Task list</h3>
+		  <ul class="task-list">
+			  <li class="task task_complete">Создать пользователя</li>
+			  <li class="task task_complete">Войдите в систему с помощью адреса электронной почты и пароля</li>
+			  <li class="task task_complete">Получить текущего вошедшего в систему пользователя</li>
+			  <li class="task task_complete">Выход из системы</li>
+			  <li class="task">Получить профиль пользователя</li>
+			  <li class="task">Получите информацию о профиле пользователя, зависящую от поставщика услуг.</li>
+			  <li class="task">Обновить профиль пользователя</li>
+			  <li class="task">Отправить пользователю электронное письмо с подтверждением</li>
+			  <li class="task">Установить пароль пользователя</li>
+			  <li class="task">Отправить электронное письмо для сброса пароля</li>
+			  <li class="task">Удаление пользователя</li>
+		  </ul>
+	  </aside>
+  </main>
 </template>
 
 <style lang="stylus">
 input
 	border 1px solid coral
+
+.grid
+	display grid
+	grid-template-columns 2fr 1fr
+	gap 32px
+
+.task-list
+	padding: 0
+	list-style-type: none;
+
+.task
+	padding-left: 24px
+	text-indent: -24px;
+	&::before
+		color tomato
+		content "☓"
+		padding-right 10px
+.task_complete
+	&::before
+		color green
+		content "✓"
 </style>
